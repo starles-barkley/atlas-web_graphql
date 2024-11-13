02be28214161f9ef5dc559e260804a03deb1fd3c
Starlee Jiles <starlee.jiles@icloud.com>
2024-11-13 22:28:52 +0000

    update app.js to connect to MongoDB

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const schema = require('./schema');
+
+// Import Task and Project models
+const Task = require('./models/task');
+const Project = require('./models/project');
 
 const app = express();
 
+// MongoDB Atlas connection string (replace <username>, <password>, <dbname> with your info)
+const dbURI = 'mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+// Connect to MongoDB Atlas
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(err => {
+    console.log('Error connecting to database:', err);
+  });
+
+// GraphQL middleware setup
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
 
+// Start the server
 app.listen(4000, () => {
-  console.log('Now listening for requests on port 4000');
+  console.log('Server running on http://localhost:4000/graphql');
 });
