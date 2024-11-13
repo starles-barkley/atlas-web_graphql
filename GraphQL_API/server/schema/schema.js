22fa106c2a54413a6b17adf5cf9c17ad44e89c04
Starlee Jiles <starlee.jiles@icloud.com>
2024-11-13 22:32:21 +0000

    update schema to include mutation and addproject mutation

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,5 +1,4 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID, GraphQLList, GraphQLSchema } = require('graphql');
-const _ = require('lodash');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID, GraphQLList, GraphQLNonNull, GraphQLSchema } = require('graphql');
 const Task = require('./models/task');
 const Project = require('./models/project');
 
@@ -72,7 +71,53 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+// Define the Mutation type
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    // Add a new project
+    addProject: {
+      type: ProjectType,
+      args: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        weight: { type: new GraphQLNonNull(GraphQLInt) },
+        description: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parent, args) {
+        // Create a new Project and save it to the database
+        const project = new Project({
+          title: args.title,
+          weight: args.weight,
+          description: args.description
+        });
+        return project.save(); // Save and return the created project
+      }
+    },
+    // Add a new task
+    addTask: {
+      type: TaskType,
+      args: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        weight: { type: new GraphQLNonNull(GraphQLInt) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
+        projectId: { type: new GraphQLNonNull(GraphQLID) }
+      },
+      resolve(parent, args) {
+        // Create a new Task and save it to the database
+        const task = new Task({
+          title: args.title,
+          weight: args.weight,
+          description: args.description,
+          projectId: args.projectId
+        });
+        return task.save(); // Save and return the created task
+      }
+    }
+  }
+});
+
 // Export the GraphQL schema
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation
 });
