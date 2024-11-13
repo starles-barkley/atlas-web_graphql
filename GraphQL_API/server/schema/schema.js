4ccdb147a5a043679292e82629f2b5410bec6a25
Starlee Jiles <starlee.jiles@icloud.com>
2024-11-13 22:29:51 +0000

    remove dummy data from schema and use MongoDB instead

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,40 +1,7 @@
-// Import required GraphQL types and lodash
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID, GraphQLList, GraphQLSchema } = require('graphql');
 const _ = require('lodash');
-
-// Create dummy data for tasks with projectId added
-const tasks = [
-  {
-    id: '1',
-    title: 'Create your first webpage',
-    weight: 1,
-    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
-    projectId: '1' // Added projectId
-  },
-  {
-    id: '2',
-    title: 'Structure your webpage',
-    weight: 1,
-    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
-    projectId: '1' // Added projectId
-  }
-];
-
-// Create dummy data for projects
-const projects = [
-  {
-    id: '1',
-    title: 'Advanced HTML',
-    weight: 1,
-    description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
-  },
-  {
-    id: '2',
-    title: 'Bootstrap',
-    weight: 1,
-    description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
-  }
-];
+const Task = require('./models/task');
+const Project = require('./models/project');
 
 // Define the TaskType
 const TaskType = new GraphQLObjectType({
@@ -44,10 +11,11 @@ const TaskType = new GraphQLObjectType({
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
     description: { type: GraphQLString },
-    project: { // Added project field in TaskType
+    project: {
       type: ProjectType,
       resolve(parent, args) {
-        return _.find(projects, { id: parent.projectId }); // Find project by projectId
+        // Find the project related to the task using projectId
+        return Project.findById(parent.projectId);
       }
     }
   })
@@ -61,10 +29,11 @@ const ProjectType = new GraphQLObjectType({
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
     description: { type: GraphQLString },
-    tasks: { // Added tasks field in ProjectType
+    tasks: {
       type: new GraphQLList(TaskType),
       resolve(parent, args) {
-        return _.filter(tasks, { projectId: parent.id }); // Filter tasks by projectId
+        // Return all tasks related to the project
+        return Task.find({ projectId: parent.id });
       }
     }
   })
@@ -76,34 +45,34 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     task: {
       type: TaskType,
-      args: { id: { type: GraphQLID } }, // Argument to filter by task id
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(tasks, { id: args.id });
+        return Task.findById(args.id);
       }
     },
     project: {
       type: ProjectType,
-      args: { id: { type: GraphQLID } }, // Argument to filter by project id
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(projects, { id: args.id });
+        return Project.findById(args.id);
       }
     },
-    tasks: { // New field to return all tasks
+    tasks: {
       type: new GraphQLList(TaskType),
       resolve(parent, args) {
-        return tasks; // Return all tasks
+        return Task.find(); // Return all tasks
       }
     },
-    projects: { // New field to return all projects
+    projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects; // Return all projects
+        return Project.find(); // Return all projects
       }
     }
   }
 });
 
-// Export the GraphQL schema with the RootQuery
+// Export the GraphQL schema
 module.exports = new GraphQLSchema({
   query: RootQuery
 });
